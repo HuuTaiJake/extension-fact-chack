3324e1fc3f6668e1044668385b18f84cd532533d
HuuTaiJake <taibui2021hcm@gmail.com>
2023-12-20 18:25:31 +0700

    Remove unnecessary

--- (extension)/extension/js/background.js
+++ (extension)/extension/js/background.js
@@ -1,51 +1,6 @@
 // background.js
 
 let menuExists = false;
-let highlightedTextToPopup = null;
-let portToPopup;
-
-// Check if the context menu item already exists before creating it
-
-// Listen for messages from other parts of the extension or content scripts
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  // Check if a message is requesting to check the existence of the context menu item
-  if (request.checkMenuExists) {
-    console.log("Checking menu existence:", menuExists);
-    sendResponse({ menuExists: menuExists });
-  }
-
-  // Check if a message is requesting highlighted text
-  if (request.getHighlightedText) {
-    // Retrieve the highlighted text from storage or any other source
-    sendResponse({ highlightedText: highlightedTextToPopup });
-  }
-});
-
-
-// Check if the context menu item exists
-chrome.contextMenus.onClicked.addListener(function (info) {
-  if (info.menuItemId === "showPopup" && info.selectionText) {
-    console.log("Context menu item clicked!");
-    console.log("Highlighted text:", info.selectionText);
-
-    // // Store the highlighted text in chrome.storage
-    // chrome.storage.local.set({ highlightedText: info.selectionText });
-
-    // Store the highlighted text
-    highlightedTextToPopup = info.selectionText;
-
-    // Send the highlighted text to the popup script through the established connection
-    if (portToPopup) {
-      portToPopup.postMessage({ highlightedText: info.selectionText });
-    }
-
-    // Send a message to the content script
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {highlightedText: info.selectionText});
-    });
-  }
-});
-
 // Create the context menu item if it doesn't exist
 if (!menuExists) {
   chrome.contextMenus.create({
@@ -55,16 +10,6 @@ if (!menuExists) {
   });
   menuExists = true; // Set the flag to true after creating the menu
 }
-
-
-// Handle request for highlighted text
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.getHighlightedText) {
-    // Retrieve the highlighted text from storage or any other source
-    const highlightedText = "This is the highlighted text.";
-    sendResponse({ highlightedText: highlightedText });
-  }
-});
 
 // Handle context menu item click
 chrome.contextMenus.onClicked.addListener(function (info) {
@@ -82,8 +27,6 @@ chrome.contextMenus.onClicked.addListener(function (info) {
   }
 });
 
-
-// ... (your existing code)
 // background.js
 
 console.log("Background script initialized");
--- (extension)/extension/js/content.js
+++
@@ -1,7 +0,0 @@
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.highlightedText) {
-      // Send the message to your web page
-      window.postMessage({highlightedText: request.highlightedText}, "*");
-    }
-  });
